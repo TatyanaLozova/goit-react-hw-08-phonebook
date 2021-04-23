9d19d16dbe6b623ad932d7614e2a525a59b93a3f
Tatyana Loza <jes220@ukr.net>
2021-04-23 22:44:42 +0300

    +

--- src/redux/auth/auth-reducer.js
+++ src/redux/auth/auth-reducer.js
@@ -1,39 +1,39 @@
-import { combineReducers } from "redux";
-import { createReducer } from "@reduxjs/toolkit";
-import autActions from './auth-actions'
+// import { combineReducers } from "redux";
+// import { createReducer } from "@reduxjs/toolkit";
+// import autActions from './auth-actions'
 
 
-const initialUserState = { name: null, email: null };
+// const initialUserState = { name: null, email: null };
 
 
-const user = createReducer(initialUserState, {
-    [autActions.registerSuccess]: (_, {payload}) => payload.user,
-    [autActions.loginSuccess]: (_, {payload}) => payload.user,
-    [autActions.logoutSuccess]: () => initialUserState,
-    [autActions.getCurrentUserSuccess]: (_, {payload}) => payload.user,
+// const user = createReducer(initialUserState, {
+//     [autActions.registerSuccess]: (_, {payload}) => payload.user,
+//     [autActions.loginSuccess]: (_, {payload}) => payload.user,
+//     [autActions.logoutSuccess]: () => initialUserState,
+//     [autActions.getCurrentUserSuccess]: (_, {payload}) => payload.user,
     
-});
-
-const token = createReducer(null, {
-    [autActions.registerSuccess]: (_, { payload }) => payload.token,
-    [autActions.loginSuccess]: (_, { payload }) => payload.token,
-    [autActions.loginSuccess]: () => null,
-});
-
-// const error = createReducer(null, {
-//     [autActions.registerError]: (_, {payload}) => payload.error,
 // });
- const isAuthenticated = createReducer(false, {
-  [autActions.registerSuccess]: () => true,
-  [autActions.loginSuccess]: () => true,
-  [autActions.getCurrentUserSuccess]: () => true,
-  [autActions.registerError]: () => false,
-  [autActions.loginError]: () => false,
-  [autActions.getCurrentUserError]: () => false,
-  [autActions.logoutSuccess]: () => false,
-});
-export default combineReducers({
-    user,
-    token,
-    isAuthenticated,
-});
+
+// const token = createReducer(null, {
+//     [autActions.registerSuccess]: (_, { payload }) => payload.token,
+//     [autActions.loginSuccess]: (_, { payload }) => payload.token,
+//     [autActions.loginSuccess]: () => null,
+// });
+
+// // const error = createReducer(null, {
+// //     [autActions.registerError]: (_, {payload}) => payload.error,
+// // });
+//  const isAuthenticated = createReducer(false, {
+//   [autActions.registerSuccess]: () => true,
+//   [autActions.loginSuccess]: () => true,
+//   [autActions.getCurrentUserSuccess]: () => true,
+//   [autActions.registerError]: () => false,
+//   [autActions.loginError]: () => false,
+//   [autActions.getCurrentUserError]: () => false,
+//   [autActions.logoutSuccess]: () => false,
+// });
+// export default combineReducers({
+//     user,
+//     token,
+//     isAuthenticated,
+// });
