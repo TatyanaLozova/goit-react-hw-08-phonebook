ad40b32f754c64cd122f1e7eb2ad7b6eb83b2b03
Tatyana Loza <jes220@ukr.net>
2021-04-26 01:13:30 +0300

    +

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -6,31 +6,31 @@ import {
     loginRequest,
     loginSuccess,
     loginError,
-    logoutRequest,
-    logoutSuccess,
-    logoutError,
-     getCurrentUserRequest,
-  getCurrentUserSuccess,
-  getCurrentUserError,
+    // logoutRequest,
+    // logoutSuccess,
+    // logoutError,
+//      getCurrentUserRequest,
+//   getCurrentUserSuccess,
+//   getCurrentUserError,
 } from './auth-actions.js'
 
-axios.defaults.baseURL = "https://connections-api.herokuapp.com/docs";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
-const token = {
-    set(token) {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    },
-    unset() {
-        axios.defaults.headers.common.Authorization = '';
-    },
-};
+// const token = {
+//     set(token) {
+//         axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//     },
+//     unset() {
+//         axios.defaults.headers.common.Authorization = '';
+//     },
+// };
 
 // POST ​/users​/signup Создать нового пользователя
 const register =( credentials) => async (dispatch) => {
     dispatch(registerRequest());
     try {
         const response = await axios.post('/users/signup', credentials);
-        token.set(response.data.token);
+        // token.set(response.data.token);
         dispatch(registerSuccess(response.data));
         
     } catch (error) {
@@ -44,7 +44,7 @@ const login= (credentials) => async (dispatch) => {
     dispatch(loginRequest());
     try {
         const response = await axios.post('/users/login', credentials);
-         token.set(response.data.token);
+        //  token.set(response.data.token);
         dispatch(loginSuccess(response.data));
 
         
@@ -54,49 +54,49 @@ const login= (credentials) => async (dispatch) => {
     }
 };
 
-// POST ​/users​/logout Разлогинить пользователя
-const logout= () => async (dispatch) => {
-    dispatch(logoutRequest());
-    try {
-        await axios.post('/users/logout');
-        token.unset();
-        dispatch(logoutSuccess())
+// // POST ​/users​/logout Разлогинить пользователя
+// const logout= () => async (dispatch) => {
+//     dispatch(logoutRequest());
+//     try {
+//         await axios.post('/users/logout');
+//         // token.unset();
+//         dispatch(logoutSuccess())
 
         
-    } catch (error) {
-        dispatch(logoutError(error.message))
+//     } catch (error) {
+//         dispatch(logoutError(error.message))
         
-    }
-};
+//     }
+// };
 
 // GET​/users​/current
 // Получить информацию о текущем пользователе
-const getCurrentUser = () => async (dispatch, getState) => {
-    const {
-        auth: { token: storageToken },
-    } = getState()
+// const getCurrentUser = () => async (dispatch, getState) => {
+//     const {
+//         auth: { token: storageToken },
+//     } = getState()
 
-    if (!storageToken) {
-        return;
-    }
-    token.set(storageToken);
+//     if (!storageToken) {
+//         return;
+//     }
+//     // token.set(storageToken);
 
-    dispatch(getCurrentUserRequest())
-    try {
-        const response = await axios.get('users/current');
+//     dispatch(getCurrentUserRequest())
+//     try {
+//         const response = await axios.get('users/current');
       
-        dispatch(getCurrentUserSuccess(response.data));
-    } catch (error) {
-        dispatch(getCurrentUserError(error.message));
-    }
-};
+//         dispatch(getCurrentUserSuccess(response.data));
+//     } catch (error) {
+//         dispatch(getCurrentUserError(error.message));
+//     }
+// };
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     register,
     login,
-    logout,
-    getCurrentUser
+    // logout,
+    // getCurrentUser
 };
 
 
